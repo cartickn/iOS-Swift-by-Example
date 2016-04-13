0022c23b68bd58fe5de9b9b2d8201f4c7f69b154
Karthik Murugesan <cartic@gmail.com>
2016-04-12 20:49:04 -0700

    Setup Alamofire Pods

---
+++ Processing Animation/Processing Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Processing Animation
+//
+//  Created by Karthik Murugesan on 4/12/16.
+//  Copyright © 2016 Karthik Murugesan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
